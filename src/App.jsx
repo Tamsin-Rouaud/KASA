db440b7b30e06a7bb46a15b8ed70ddc79a311386
Tamsin_Rouaud <tamsinrouaud83@gmail.com>
2024-01-07 19:01:19 +0100

    Initialisation du projet KASA

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.sass'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <p> Hello world</p>
+      </div>
+      
+    
+  )
+}
+
+export default App
---
+++ src/Router.js
@@ -0,0 +1,20 @@
+// Router.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home"; // Importez vos différentes pages
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
