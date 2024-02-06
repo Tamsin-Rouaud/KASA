2032e53441eb7bc81b07cd2d54a58c31219337be
Tamsin_Rouaud <tamsinrouaud83@gmail.com>
2024-02-06 14:14:07 +0100

    Enfin à jour

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,30 +1,33 @@
 import './Collapse.sass';
 import { useState } from 'react';
-import arrowBack from '../../assets/images/arrowBack.png'
+import arrowBack from '../../assets/images/arrowBack.png';
 
-
-const Collapse = ({ title, content}) => {
+const Collapse = ({ title, content }) => {
   const [isCollapsed, setCollapsed] = useState(true);
-  const arrow = arrowBack
+  const arrow = arrowBack;
+
   const toggleCollapse = () => {
     setCollapsed(!isCollapsed);
   };
 
-
-
-return (
-  <div className={`collapse${isCollapsed ? '__closed' : ''}`}>
-    <div className="collapse__header" onClick={toggleCollapse}>
+  return (
+    <div className={`collapse${isCollapsed ? '__closed' : ''}`}>
+      <div className="collapse__header" onClick={toggleCollapse}>
         <span className="collapse__title">{title}</span>
-        <img className={`collapse__arrow${isCollapsed ? '--up' : '--down'}`} src={arrow} alt='Voir le contenu'/>
-    </div>
-     {isCollapsed ? null : (
-    <div className="collapse__container__content">
-      <div className={`collapse__content${isCollapsed ? '--hidden' : ''}`}>{content}
+        <img
+          className={`collapse__arrow${isCollapsed ? '--up' : '--down'}`}
+          src={arrow}
+          alt='Voir le contenu'
+        />
+      </div>
+
+      <div className={`collapse__container__content${isCollapsed ? '--hidden' : ''}`}>
+        <div className="collapse__container__content--text">
+          {content}
+        </div>
       </div>
     </div>
-     )}
-  </div>
-);
+  );
 };
-export default Collapse
+
+export default Collapse;
